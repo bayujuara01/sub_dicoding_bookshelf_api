aa3dc383bcc2bb5ab1e5472830ffbdd7e00c26ef
bayujuara01 <ariefyantobayu@gmail.com>
2021-04-18 15:21:50 +0700

    rename to bookhandler

--- src/api/books/index.js
+++ src/api/books/index.js
@@ -1,11 +1,11 @@
-const NotesHandler = require('./handler');
+const BooksHandler = require('./handler');
 const routes = require('./routes');
 
 module.exports = {
   name: 'books',
   version: '1.0.1',
   register: async (server, { service }) => {
-    const noteHandler = new NotesHandler(service);
+    const noteHandler = new BooksHandler(service);
     server.route(routes(noteHandler));
   }
 };
